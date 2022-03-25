9857b2948ce1fd22491a886a17accaadec494b6a
farisulhaq <faries089@gmail.com>
2022-03-25 08:17:48 +0700

    edit tampilan

--- FormSelected/index.php
+++ FormSelected/index.php
@@ -15,31 +15,50 @@ $datas = mysqli_query($conn, $sql);
 
 <body>
     <?php $menuPilih = isset($_POST['menu']) ? $_POST['menu'] : '' ?>
-    <form action="" method="post">
-        <select name="menu" id="menu" onchange="this.form.submit();">
-            <option value="-- ! --">-- ! --</option>
-            <?php foreach ($datas as $data) : ?>
-                <option value="<?= $data['kategori'] ?>" <?php ($menuPilih == $data['kategori']) ? print 'selected' : print '' ?>><?= $data['kategori'] ?></option>
-            <?php endforeach; ?>
-        </select>
-        <!-- <input type="submit" value="submit" name="submit" <?php $menuPilih != '' ? print 'hidden' : print '' ?>> -->
-    </form>
-    <?php if ($menuPilih) : ?>
-        <?php $datas = mysqli_query($conn, "SELECT nama_menu, harga FROM menu WHERE kategori = '$menuPilih'") ?>
-        <form action="bayar.php" method="post">
-            <select name="menu" id="menu">
-                <option value="-- ! --">-- ! --</option>
-                <?php foreach ($datas as $data) : ?>
-                    <option value="<?= $data['nama_menu'] ?>" <?php ($menuPilih == $data['nama_menu']) ? print 'selected' : print '' ?> name="menu"><?= $data['nama_menu'] ?></option>
-                <?php endforeach; ?>
-            </select>
-            <br>
-            <!-- <input type="text" value="<?= $data['harga'] ?>"> -->
-            <input type="text" placeholder="Jumlah Pesanan" name="jumlah">
-            <br>
-            <input type="submit" value="bayar" name="submit">
-        </form>
-    <?php endif ?>
+    <table>
+        <tr>
+            <td><label for="menu_kategori">Pilih Kategori</label></td>
+            <td>
+                <form action="" method="post">
+                    <select name="menu" id="menu_kategori" onchange="this.form.submit();">
+                        <option value="-- ! --">-- ! --</option>
+                        <?php foreach ($datas as $data) : ?>
+                            <option value="<?= $data['kategori'] ?>" <?php ($menuPilih == $data['kategori']) ? print 'selected' : print '' ?>><?= $data['kategori'] ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    <!-- <input type="submit" value="submit" name="submit" <?php $menuPilih != '' ? print 'hidden' : print '' ?>> -->
+                </form>
+            </td>
+        </tr>
+        <?php if ($menuPilih) : ?>
+            <?php $datas = mysqli_query($conn, "SELECT nama_menu, harga FROM menu WHERE kategori = '$menuPilih'") ?>
+            <form action="bayar.php" method="post">
+                <tr>
+                    <td><label for="menu">Pilih Menu</label></td>
+                    <td>
+                        <select name="menu" id="menu">
+                            <option value="-- ! --">-- ! --</option>
+                            <?php foreach ($datas as $data) : ?>
+                                <option value="<?= $data['nama_menu'] ?>" <?php ($menuPilih == $data['nama_menu']) ? print 'selected' : print '' ?> name="menu"><?= $data['nama_menu'] ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="jumlah">Jumlah</label></td>
+                    <td>
+                        <input type="text" placeholder="Jumlah Pesanan" id="jumlah" name="jumlah">
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type="submit" value="bayar" name="submit">
+                    </td>
+                </tr>
+            </form>
+        <?php endif ?>
+    </table>
 </body>
 
 </html>
