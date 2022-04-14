df5ee1300b9b8eb079bbc8c0a3a28744d63c6994
farisulhaq <faries089@gmail.com>
2022-04-15 00:29:19 +0700

    validasi checkboxs

--- coba/index.php
+++ coba/index.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="action.php" method="post">
-        <input type="text" name="nama">
-        <input type="text" name="alamat">
-        <input type="submit" value="Click" name="submit">
-    </form>
-</body>
-</html>
+<?php 
+// contoh array 1
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+// contoh array 2
+$benda = ["komputer", "printer", "laptop"];
+echo "<br>";
+echo "Saya mempunyai $benda[0], $benda[1] dan $benda[2]";
+?>
---
+++ praktikum/paw/index.php
@@ -0,0 +1,13 @@
+<?php
+// fungsi variabel local
+
+function myTest()
+{
+    $x = 0;
+    echo "\n x = $x";
+    $x++;
+}
+
+myTest();
+myTest();
+myTest();
